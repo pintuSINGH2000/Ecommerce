fa292c0fcb690326793d79a4e4def503fd2a7037
pintuSINGH2000 <94526600+pintuSINGH2000@users.noreply.github.com>
2023-09-20 12:02:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname),'./client/build'))
+app.use(express.static(path.join(__dirname,'./client/build')));
 
 //routes
 app.use('/api/v1/auth',authRoutes);
